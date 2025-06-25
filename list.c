2e59bd9c4810b514a8f7884403ed4ca1ac650a3d
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2025-06-25 20:05:05 -0300

    eu nem vou remover disso né?

--- list.c
+++ list.c
@@ -16,14 +16,6 @@ list_t* create(){
     return lisa;
 }
 
-int search(list_t* L, char* val) {
-    for (int i = 0; i < L->tam; i++) {
-        if (strcmp(L->elementar[i], val) == 0) {
-            return i;
-        }
-    }
-    return -1;}
-
 void addlist(list_t* L, char* val) {
     if (L->tam >= L->cap) {
         L->cap *= 2;
@@ -33,22 +25,10 @@ void addlist(list_t* L, char* val) {
     L->tam++;
 }
 
-char* remover(list_t* L, char* val) {
-    int position = search(L, val);
-    if (position == -1) return NULL;
-
-    char* removed = L->elementar[position];
-    for (int i = position; i < L->tam - 1; i++) {
-        L->elementar[i] = L->elementar[i + 1];
-    }
-    L->tam--;
-    return removed;}
-
 int listless(list_t* L){return L->tam == 0;}
 
 void history(list_t* l){
-    for (int i = 0; i < l->tam; i++){
-        printf("%s\n", l->elementar[i]);
+    for (int i = 0; i < l->tam; i++){printf("%s\n", l->elementar[i]);
     }}
 
 int tama(list_t* l){return l->tam;}
@@ -56,5 +36,4 @@ int tama(list_t* l){return l->tam;}
 void destruction(list_t* lia){
     for (int i = 0; i < lia->tam; i++) {free(lia->elementar[i]);}
     free(lia->elementar);
-    free(lia);
-}
+    free(lia);}
