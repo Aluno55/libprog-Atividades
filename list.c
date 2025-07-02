0f792642e5f14ba45ea4caf8f50cf80ffa84bda9
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2025-07-02 14:15:52 -0300

    list

--- list.c
+++ list.c
@@ -13,27 +13,18 @@ list_t* create(){
     lisa->elementar = (char**) malloc(sizeof(char*)*1);
     lisa->tam = 0;
     lisa->cap = 1;
-    return lisa;
-}
+    return lisa;}
 
 void addlist(list_t* L, char* val) {
-    if (L->tam >= L->cap) {
-        L->cap *= 2;
-        L->elementar = realloc(L->elementar, sizeof(char*) * L->cap);
-    }
+    if (L->tam >= L->cap) {L->cap *= 2;
+        L->elementar = realloc(L->elementar, sizeof(char*) * L->cap);}
     L->elementar[L->tam] = strdup(val);
-    L->tam++;
-}
+    L->tam++;}
 
-int listless(list_t* L){return L->tam == 0;}
-
-void history(list_t* l){
-    for (int i = 0; i < l->tam; i++){printf("%s\n", l->elementar[i]);
-    }}
+void history(list_t* l){for (int i = 0; i < l->tam; i++){printf("%s\n", l->elementar[i]);}}
 
 int tama(list_t* l){return l->tam;}
 
 void destruction(list_t* lia){
     for (int i = 0; i < lia->tam; i++) {free(lia->elementar[i]);}
-    free(lia->elementar);
-    free(lia);}
+    free(lia->elementar);free(lia);}
